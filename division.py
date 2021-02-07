7535706d1e17ecfbf27878c1edbedd5b13f7087f
unknown <pateldhwani59@gmail.com>
2021-02-06 17:26:04 -0800

    added zeroDivisionError

--- division.py
+++ division.py
@@ -2,6 +2,15 @@ def div(thislist):
     answer = thislist[0]
     for x in (thislist[1:]):
         x = float(x)
-        answer /= x
-    return(answer)    
+
+        try:
+            answer /= x
+        except ZeroDivisionError:
+            print("division by zero is impossible") 
+        else:
+            return(answer)
+
+
+        
+        
    
